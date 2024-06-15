4a6e78f5fad4b7da879d1aae3aae2719dbf6df1b
Hannes <33062605+HannesGitH@users.noreply.github.com>
2024-06-15 17:45:40 +0200

    integrated graph into model

--- lib/models/countryGraph.dart
+++ lib/models/countryGraph.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:neighboring_countries/neighboring_countries.dart' as nc;
-import 'package:country_picker/country_picker.dart'; // as cp; //unfortunately alias wont work: https://github.com/rrousselGit/freezed/issues/519
+import 'package:country_picker/country_picker.dart'
+    as cp; //unfortunately alias wont work with freezed: https://github.com/rrousselGit/freezed/issues/519
 
 part 'countryGraph.g.dart';
 part 'countryGraph.freezed.dart';
@@ -13,18 +14,27 @@ class SelectedCountriesModel extends _$SelectedCountriesModel {
 
   @override
   SelectedCountriesModelState build() {
-    final possibleCountries =
-        graph.nodes.map((c) => c.toPickerCountry()).whereType<Country>();
+    final possibleCountries = _asCountries(graph.nodes);
+    graph.neighbors.listen(_graphListener);
     return SelectedCountriesModelState(possible: possibleCountries);
   }
 
+  void _graphListener(countries) {
+    state = state.copyWith(suggestions: _asCountries(countries));
+  }
+
+  Iterable<Country> _asCountries(Iterable<nc.Country> countries) =>
+      countries.map((c) => c.toCountry()).whereType<Country>();
+
   void select(Country country) {
     state = state.copyWith(selection: [...state.selection, country]);
+    graph.selectNode(country._node);
   }
 
   void deselect(Country country) {
     state =
         state.copyWith(selection: state.selection.where((c) => c != country));
+    graph.unselectNode(country._node);
   }
 
   void toggle(Country country) {
@@ -42,9 +52,11 @@ class SelectedCountriesModel extends _$SelectedCountriesModel {
   }
 
   void clearSearch() {
-    state = state.copyWith(suggestions: []);
+    // mocks a new selection as this overrides the suggestions
+    _graphListener(graph.currentNeighbors);
   }
 
+  @deprecated
   void clearSelection() {
     state = state.copyWith(selection: []);
   }
@@ -59,8 +71,18 @@ class SelectedCountriesModelState with _$SelectedCountriesModelState {
   }) = _SelectedCountriesModelState;
 }
 
-extension ToPickerCountry on nc.Country {
-  Country? toPickerCountry() => CountryParser.tryParse(name);
+//circumvent freezed alias problems
+class Country extends cp.Country {
+  Country.fromJson(this._node, {required super.json}) : super.from();
+  final nc.Country _node;
+}
+
+extension ToCountry on nc.Country {
+  // unfortunately (as upposed to e.g. js prototype based approach) we cannot cast to a subclass
+  // that is why we take the umweg via json (i hate it but it works)
+  Country? toCountry() => ((Map<String, dynamic>? json) =>
+          json == null ? null : Country.fromJson(this, json: json))(
+      cp.CountryParser.tryParse(name)?.toJson());
 }
 
 extension FlagIconWidget on Country {
